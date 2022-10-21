0138031c6a560e9db65d4b672b5afbfc255a13c9
SGS-J <santigarsan07@gmail.com>
2022-10-21 13:21:22 -0500

    First commit

---
+++ formulario/frm_aplicar.js
@@ -0,0 +1,13 @@
+const pageForm = document.querySelector('form');
+
+pageForm.addEventListener('submit', (e) => {
+  const formData = new FormData(pageForm);
+
+  fetch('http://localhost:8080/user/add', {
+    body: formData,
+    method: 'POST',
+  }).then((res) => {
+    location.reload();
+  });
+  e.preventDefault();
+});
