9c100555ec0126df4d8a236cdc7d7ba934cd4b5f
SGS-J <santigarsan07@gmail.com>
2022-10-22 12:19:36 -0500

    Config submit listener

--- formulario/frm_aplicar.js
+++ formulario/frm_aplicar.js
@@ -1,12 +1,18 @@
 const pageForm = document.querySelector('form');
+const btn = document.querySelector('.btn-submit');
 
 pageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+});
+
+btn.addEventListener('click', (e) => {
   const formData = new FormData(pageForm);
 
   fetch('http://localhost:8080/user/add', {
     body: formData,
     method: 'POST',
+  }).catch((error) => {
+    console.log(error);
   });
   /*.then((res) => {
     location.reload();
