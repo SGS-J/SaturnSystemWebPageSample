e861b1bdc38f274f559a5c8a4bb09ee4e1afdb63
SGS-J <santigarsan07@gmail.com>
2022-10-24 11:56:11 -0500

    Set up log

--- formulario/frm_aplicar.js
+++ formulario/frm_aplicar.js
@@ -8,13 +8,16 @@ pageForm.addEventListener('submit', (e) => {
 btn.addEventListener('click', (e) => {
   const formData = new FormData(pageForm);
 
-  const res = fetch('http://localhost:8080/user/add', {
+  fetch('http://localhost:8080/user/add', {
     body: formData,
     method: 'POST',
-  }).catch((error) => {
-    console.log(error);
-  });
-  console.log(res);
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   /*.then((res) => {
     location.reload();
   });+*/
