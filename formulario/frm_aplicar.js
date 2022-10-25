f9a24157a0489ab36b31780802b8820c2e8e7d04
SGS-J <santigarsan07@gmail.com>
2022-10-24 21:20:22 -0500

    Set location to reload when submit form

--- formulario/frm_aplicar.js
+++ formulario/frm_aplicar.js
@@ -3,24 +3,13 @@ const btn = document.querySelector('.btn-submit');
 
 pageForm.addEventListener('submit', (e) => {
   e.preventDefault();
-});
 
-btn.addEventListener('click', (e) => {
   const formData = new FormData(pageForm);
 
   fetch('http://localhost:8080/user/add', {
     body: formData,
     method: 'POST',
-  })
-    .then((res) => {
-      res.text().then((txt) => {
-        console.log(txt);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  /*.then((res) => {
+  }).then((res) => {
     location.reload();
-  });+*/
+  });
 });
